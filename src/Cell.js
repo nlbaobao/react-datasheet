3d8fc9579a94625d89c142b290f5f6fc9b7b956a
chilin <nl390338@alibaba-inc.com>
2023-06-14 17:17:15 +0800

    ## 增加功能 - 实现了用户登录- 在登录页面添加了用户名和密码输入框，用户可以输入用户名和密码进行登录。 - 实现了与后端API的交互，验证用户输入的用户名和密码是否正确。 - 在登录成功后，跳转到用户的个人主页 Fixes #123 chore:update version

--- src/Cell.js
+++ src/Cell.js
@@ -12,7 +12,7 @@ export default class Cell extends PureComponent {
 
     const {colSpan, rowSpan} = cell
     const attributes = attributesRenderer ? attributesRenderer(cell, row, col) : {}
-        console.log(1111222)
+        console.log(111)
     return (
       <td
         className={className}
